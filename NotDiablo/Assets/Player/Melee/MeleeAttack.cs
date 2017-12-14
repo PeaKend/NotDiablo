75bd290ef14a21dc236206a44d540291195a9af2
dea4493 <dea4493@gmail.com>
2017-12-14 06:30:09 -0300

    Classes
    HAZ CLASSES

---
+++ NotDiablo/Assets/Player/Melee/MeleeAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour {
+
+	// Use this for initialization
+	int duration = 30;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(duration > 0) duration--;
+		else Destroy(gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Debug.Log(col.tag);
+		if(col.tag != "Player") Destroy(col.gameObject);
+	}
+}
